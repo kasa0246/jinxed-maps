ac26d659a09509961757ae21d386ed8019a8b08e
kasa0246 <karoseria99@gmail.com>
2025-05-13 13:47:08 +0200

    Update the_dust.pas
    description update

--- ui/mods/kasa/systems/the_dust.pas
+++ ui/mods/kasa/systems/the_dust.pas
@@ -1,6 +1,6 @@
 {
     "name": "The Dust",
-    "description": "The Blight prevails, slowly turning Earth into a desert. K.A.S.A. sends the so-called 'Aight' mission through a wormhole to another dimension in hope to find a better place for humans to live. The clock is ticking, though. OR is it?",
+    "description": "The Blight spreads unchecked, slowly transforming Earth into a barren wasteland. The air grows thin. The crops grow dry. With time and options running out, K.A.S.A. launches the Aight mission — a desperate voyage through a wormhole in search of a new home for humanity. The clock is ticking, though. OR is it?",
     "creator": "kasa",
     "version": "1.0.0",
     "planets": [
